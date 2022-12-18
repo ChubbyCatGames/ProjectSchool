01e439f841a1544b3ee095b2217b1865cfea17f4
elibo09 <e.barba.2019@alumnos.urjc.es>
2022-12-18 18:05:18 +0100

    Update ghostBehaviour.cs

--- Assets/Script/ghostBehaviour.cs
+++ Assets/Script/ghostBehaviour.cs
@@ -13,17 +13,30 @@ public class ghostBehaviour : MonoBehaviour
     SequenceNode root;
     SelectorNode selectorGoClass;
     LeafNode wander;
-    LeafNode inClass;
-    SelectorNode classAction;
+    SequenceNode sequenceClass;
+    LeafNode goToClass;
+    SelectorNode selectClassAction;
     LeafNode learn;
-    SequenceNode turnAround;
+    SequenceNode sequenceTurn;
     LeafNode isturnAround;
     LeafNode joking;
-    TimerDecoratorNode timerNode;
+    //TimerDecoratorNode timerNode;
 
     //Utility System
     UtilitySystemEngine us;
-    
+    public float timePee;
+    public float needPee;
+
+    public float timeEat;
+    public float needEat;
+    public float thresholdEat = 75;
+
+    //scaring for alumni, teachers room for teachers
+    public float timeGhosting;
+    public float needGhosting;
+
+    float minNeed = 0;
+    float maxNeed = 1;
 
     //Signs controller
     [SerializeField] private SignsController sc;
@@ -35,48 +48,71 @@ public class ghostBehaviour : MonoBehaviour
 
         root = ghostBT.CreateSequenceNode("Root selector", false);
         selectorGoClass = ghostBT.CreateSelectorNode("Go to class");
-        wander = ghostBT.CreateLeafNode("Wandering" );
-        inClass = ghostBT.CreateLeafNode("In class", inClassAction, checkClass);
-        classAction = ghostBT.CreateSelectorNode("Select class action");
+        wander = ghostBT.CreateSubBehaviour("Wander", us);
+        sequenceClass = ghostBT.CreateSequenceNode("Actions in class", false);
+        goToClass = ghostBT.CreateLeafNode("Walk to class", WalkToClass, ArriveToClass);
+        selectClassAction = ghostBT.CreateSelectorNode("Select class action");
         learn = ghostBT.CreateLeafNode("Learn in class", learnAction, checkLearning);
-        turnAround = ghostBT.CreateSequenceNode("Teacher sequence", false);
+        sequenceTurn = ghostBT.CreateSequenceNode("Teacher sequence", false);
         isturnAround = ghostBT.CreateLeafNode("Is tecaher turned around", isTurning, checkTurning);
         joking = ghostBT.CreateLeafNode("Joke", jokingAction, checkJoking);
-        timerNode = ghostBT.CreateTimerNode("Timer", isturnAround, 5);
+        //timerNode = ghostBT.CreateTimerNode("Timer", isturnAround, 5);
 
         //Children
-        root.AddChild(wander);
-        root.AddChild(inClass);
         root.AddChild(selectorGoClass);
 
-        selectorGoClass.AddChild(classAction);
-        selectorGoClass.AddChild(learn);
-        selectorGoClass.AddChild(turnAround);
+        selectorGoClass.AddChild(wander);
+        selectorGoClass.AddChild(sequenceClass);
 
-        turnAround.AddChild(isturnAround);
-        turnAround.AddChild(timerNode);
-        turnAround.AddChild(joking);
+        sequenceClass.AddChild(goToClass);
+        sequenceClass.AddChild(selectClassAction);
+
+        selectClassAction.AddChild(learn);
+        selectClassAction.AddChild(sequenceTurn);
+
+        sequenceTurn.AddChild(isturnAround);
+        sequenceTurn.AddChild(joking);
 
         //Root
         ghostBT.SetRootNode(root);
     }
 
-    //Métodos BT
-    public virtual void inClassAction() { }
+    //ACTIONS BT
 
-    public ReturnValues checkClass() { return ReturnValues.Succeed; } 
+    //Go to class
+    public virtual void WalkToClass() { }
+
+    public ReturnValues ArriveToClass() { return ReturnValues.Succeed; } 
     
+    //Learning
     public virtual void learnAction() { }
 
     public ReturnValues checkLearning() { return ReturnValues.Succeed; }
 
+    //Is turning?
     public virtual void isTurning() { }
 
     public ReturnValues checkTurning() { return ReturnValues.Succeed; }
     
+    //Joking
     public virtual void jokingAction() { }
 
     public ReturnValues checkJoking() { return ReturnValues.Succeed; }
+
+    private void CreateUtilitySystem()
+    {
+        us = new UtilitySystemEngine(true);
+
+        needEat = minNeed;
+        needPee = minNeed;
+        needGhosting = minNeed;
+    
+        Factor factorPee= new LeafVariable(()=>needPee, maxNeed, minNeed);//linear
+        Factor factorEat = new LeafVariable(()=>needEat, maxNeed, minNeed);//sigmoide
+        Factor factorGhosting = new LeafVariable(()=>needGhosting, maxNeed, minNeed);//umbral/threshold
+
+        Factor curvePee = new LinearCurve(factorPee, 0.01f);
+    }
 
     private void Awake()
     {
@@ -85,8 +121,8 @@ public class ghostBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-        us = new UtilitySystemEngine(true);
+        CreateBehaviourTree();
+        CreateUtilitySystem();
     }
 
     // Update is called once per frame
@@ -96,12 +132,13 @@ public class ghostBehaviour : MonoBehaviour
         us.Update();
     }
 
-    void WanderAction()//ESTE NODO ES EL SISTEMA DE UTILIDAD
+
+    //ACCIONES DEL SISTEMA DE UTILIDAD
+    void Wandering()
     {
 
     }
 
-    //ACCIONES DEL SISTEMA DE UTILIDAD
     void UrinatingAction()
     {
 
