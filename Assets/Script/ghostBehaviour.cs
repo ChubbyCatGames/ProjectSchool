f0cf07a17e7918533b68a2869dcd8d2f31eb8242
elibo09 <e.barba.2019@alumnos.urjc.es>
2022-12-18 13:56:00 +0100

    Update ghostBehaviour.cs

--- Assets/Script/ghostBehaviour.cs
+++ Assets/Script/ghostBehaviour.cs
@@ -10,9 +10,16 @@ public class ghostBehaviour : MonoBehaviour
 
     //Behaviour Tree
     BehaviourTreeEngine ghostBT;
-    SequenceNode seqNode;
+    SequenceNode root;
+    SelectorNode selectorGoClass;
     LeafNode wander;
     LeafNode inClass;
+    SelectorNode classAction;
+    LeafNode learn;
+    SequenceNode turnAround;
+    LeafNode isturnAround;
+    LeafNode joking;
+    TimerDecoratorNode timerNode;
 
     //Utility System
     UtilitySystemEngine us;
@@ -21,6 +28,55 @@ public class ghostBehaviour : MonoBehaviour
     //Signs controller
     [SerializeField] private SignsController sc;
 
+    //BT
+    private void CreateBehaviourTree()
+    {
+        ghostBT = new BehaviourTreeEngine();
+
+        root = ghostBT.CreateSequenceNode("Root selector", false);
+        selectorGoClass = ghostBT.CreateSelectorNode("Go to class");
+        wander = ghostBT.CreateLeafNode("Wandering" );
+        inClass = ghostBT.CreateLeafNode("In class", inClassAction, checkClass);
+        classAction = ghostBT.CreateSelectorNode("Select class action");
+        learn = ghostBT.CreateLeafNode("Learn in class", learnAction, checkLearning);
+        turnAround = ghostBT.CreateSequenceNode("Teacher sequence", false);
+        isturnAround = ghostBT.CreateLeafNode("Is tecaher turned around", isTurning, checkTurning);
+        joking = ghostBT.CreateLeafNode("Joke", jokingAction, checkJoking);
+        timerNode = ghostBT.CreateTimerNode("Timer", isturnAround, 5);
+
+        //Children
+        root.AddChild(wander);
+        root.AddChild(inClass);
+        root.AddChild(selectorGoClass);
+
+        selectorGoClass.AddChild(classAction);
+        selectorGoClass.AddChild(learn);
+        selectorGoClass.AddChild(turnAround);
+
+        turnAround.AddChild(isturnAround);
+        turnAround.AddChild(timerNode);
+        turnAround.AddChild(joking);
+
+        //Root
+        ghostBT.SetRootNode(root);
+    }
+
+    //Métodos BT
+    public virtual void inClassAction() { }
+
+    public ReturnValues checkClass() { return ReturnValues.Succeed; } 
+    
+    public virtual void learnAction() { }
+
+    public ReturnValues checkLearning() { return ReturnValues.Succeed; }
+
+    public virtual void isTurning() { }
+
+    public ReturnValues checkTurning() { return ReturnValues.Succeed; }
+    
+    public virtual void jokingAction() { }
+
+    public ReturnValues checkJoking() { return ReturnValues.Succeed; }
 
     private void Awake()
     {
@@ -29,8 +85,8 @@ public class ghostBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ghostBT = new BehaviourTreeEngine();
-        us = new UtilitySystemEngine();
+        
+        us = new UtilitySystemEngine(true);
     }
 
     // Update is called once per frame
