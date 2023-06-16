3fa3222ee775288924c5939c78ead9d29e46575b
NeoTX14 <js.pardo.2019@alumnos.urjc.es>
2023-06-16 18:54:56 +0200

    Janitor no flirt cuando están fuera de clase y debugs quitados

--- Assets/janitor/TestJanitor.cs
+++ Assets/janitor/TestJanitor.cs
@@ -6,25 +6,26 @@ using UnityEngine.AI;
 public class TestJanitor : MonoBehaviour
 {
     [SerializeField] private SignsController scJan;
+    [SerializeField] private SchoolScript sc;
     private NavMeshAgent meshAgent;
     private StateMachineEngine fsm;
     private string interruptedState;
 
     private ArriveToDestination arriveToDestination;
+
     //destinos
     private Vector3 posDest;
     [SerializeField] private List<GameObject> targets;
     private int n = 0;
-    private int tope;
     private Vector3 origin;
 
 
     [SerializeField] CookBehaviour cook;
 
     //para avisar a la cocinera
-    private bool enAlmacen = false;
     public bool flagForCook;
     public bool endFlirt = false;
+    public bool classRn = false;
 
 
     // Start is called before the first frame update
@@ -33,10 +34,12 @@ public class TestJanitor : MonoBehaviour
 
         fsm = new StateMachineEngine();
         meshAgent = GetComponent<NavMeshAgent>();
-        //posDest = Dest.transform.position;
         origin = meshAgent.transform.position;
         posDest = targets[n].transform.position;
-        //tope = targets.Count;
+
+        //relacion con campana
+        sc.bellEvent.AddListener(()=> classRn = true);
+        sc.bellEventEnd.AddListener(() => classRn = false);
 
         //STATES
         State idle = fsm.CreateEntryState("idle");
@@ -45,35 +48,29 @@ public class TestJanitor : MonoBehaviour
         State walkToPB = fsm.CreateState("walkToPB", WalkToPBAction);
         State flirt = fsm.CreateState("flirt", FlirtAction);
         State originAgain = fsm.CreateState("originAgain", OriginAgainAction);
-
-        // State waitingForCook = fsm.CreateState("waitingForCook", waitingForCookAction);
-        //State flirt = fsm.CreateState("flirt", BlueAction);
         State cleanS = fsm.CreateState("cleanS", CleanAction);
 
-        //Perception
+        //Perceptions
         arriveToDestination = fsm.CreatePerception<ArriveToDestination>(new ArriveToDestination());
         Perception started = fsm.CreatePerception<PushPerception>();
-    
 
-        //Transition
+        //Transitions
         fsm.CreateTransition("started", idle, started, walk);
         fsm.CreateTransition("second", walk, started, walkToPA);
 
         fsm.CreateTransition("flirt", walkToPA, started, flirt);
 
+        //si flirtea
         fsm.CreateTransition("third", flirt, started, walkToPB);
+        //si no flirtea
+        fsm.CreateTransition("thirdNoFlirt", walkToPA, started, walkToPB);
 
         fsm.CreateTransition("clean", walk, started,cleanS);
         fsm.CreateTransition("continue", cleanS, started, walk);
 
         fsm.CreateTransition("originAgain", walkToPB, started, originAgain);
         fsm.CreateTransition("restart", originAgain, started, idle);
-        //fsm.CreateTransition("nearWarehuse", walk, nearWarehuse, flirt);
-        //fsm.CreateTransition("cookEvent", flirt, cookEvent, walk);
 
-        //fsm.CreateTransition("stainAppears", walk, stainAppears, clean);
-        //fsm.CreateTransition("staincCleaned", clean, staincCleaned, walk);
-        //Debug.Log(fsm.GetCurrentState().Name);
     }
 
     // Update is called once per frame
@@ -82,64 +79,47 @@ public class TestJanitor : MonoBehaviour
         fsm.Update();
         arriveToDestination.SetPosition(transform.position);
 
-        /*Debug.Log(arriveToDestination.GetPosition());
-        Debug.Log(arriveToDestination.GetDestination());
-        Debug.Log(Vector3.Distance(arriveToDestination.GetPosition(), arriveToDestination.GetDestination()));
-        
-        if (arriveToDestination.Check2() == true)
-        {
-            //Debug.Log(n);
-            //Debug.Log(tope);
-            
-            if (n < tope - 1)
-            {
 
-                n++;
-                meshAgent.destination = targets[n].transform.position;
-
-            }
-
-        }*/
-
-        Debug.Log(fsm.GetCurrentState().Name);
+        //Debug.Log(fsm.GetCurrentState().Name);
         if (posDest != null && fsm.GetCurrentState().Name =="idle")
         {
             fsm.Fire("started");
-            //Debug.Log(fsm.GetCurrentState().Name);
         }
 
-        //Debug.Log(fsm.GetCurrentState().Name);
-        //Debug.Log(arriveToDestination.Check2());
-        //Debug.Log(Vector3.Distance(transform.position, targets[0].transform.position));
         if (Vector3.Distance(transform.position, targets[0].transform.position) < 0.8f && fsm.GetCurrentState().Name == "walk")
         {
-            Debug.Log("EjecutaSegundo");
-
             fsm.Fire("second");
-
-
-            //Debug.Log("HACIA segundo destino");
         }
 
-        //Debug.Log(Vector3.Distance(transform.position, targets[1].transform.position) < 2.0f);
-        if (fsm.GetCurrentState().Name == "walkToPA" && Vector3.Distance(transform.position, targets[1].transform.position) < 2.0f)
+        //Si están en clase flirtea, sino sale del almacen
+        if (classRn)
         {
-            Debug.Log("EN segundo destino");
-            if (endFlirt == false)
+            if (fsm.GetCurrentState().Name == "walkToPA" && Vector3.Distance(transform.position, targets[1].transform.position) < 2.0f)
             {
-                fsm.Fire("flirt");
+                Debug.Log("EN segundo destino");
+                if (endFlirt == false)
+                {
+                    fsm.Fire("flirt");
+                }
+            }
+
+            if (fsm.GetCurrentState().Name == "flirt" && Vector3.Distance(transform.position, targets[1].transform.position) < 2.0f)
+            {
+
+                if (endFlirt == true)
+                {
+                    fsm.Fire("third");
+                }
             }
         }
-
-        if (fsm.GetCurrentState().Name == "flirt" && Vector3.Distance(transform.position, targets[1].transform.position) < 2.0f)
+        else
         {
-            //Debug.Log("xd");
-            if (endFlirt == true)
+            if (fsm.GetCurrentState().Name == "walkToPA" && Vector3.Distance(transform.position, targets[1].transform.position) < 2.0f)
             {
-                fsm.Fire("third");
-                
+                    fsm.Fire("thirdNoFlirt");
             }
         }
+        
 
         if(fsm.GetCurrentState().Name == "walkToPB" && Vector3.Distance(transform.position, targets[2].transform.position) < 2.0f)
         {
@@ -152,33 +132,10 @@ public class TestJanitor : MonoBehaviour
             fsm.Fire("restart");
         }
 
-        /*
-
-        if (fsm.GetCurrentState().Name == "walkToPA" && endFlirt == false)
-        {
-            Debug.Log("EN segundo destino");
-            WaitingForCookAction();
-            
-        }
-        if (endFlirt == true && Vector3.Distance(transform.position, targets[1].transform.position) < 0.8f)
-        {
-            fsm.Fire("third");
-        }
-            */
-        //if(arriveToDestination.Check2() && fsm.GetCurrentState().Name=="")
-        /*
-        if (arriveToDestination.Check2() && flagForCook == false)
-        {
-            fsm.Fire("third");
-        }*/
-        //if llega a almcacen
-        //enAlmacen = true
-
     }
     
     private void OnEnable()
     {
-        //Debug.Log("Enable");
         controlEvento.current.ManchaCercaPj.AddListener(FireClean);
     }
     void WalkAction()
@@ -197,7 +154,6 @@ public class TestJanitor : MonoBehaviour
         meshAgent.destination = targets[n].transform.position;
 
         meshAgent.speed = 3.5f;
-        //meshAgent.destination = posDest;
         arriveToDestination.SetDestination(meshAgent.destination);
 
     }
@@ -207,7 +163,6 @@ public class TestJanitor : MonoBehaviour
         meshAgent.destination = targets[n].transform.position;
 
         meshAgent.speed = 3.5f;
-       // meshAgent.destination = posDest;
         arriveToDestination.SetDestination(meshAgent.destination);
 
     }
@@ -223,15 +178,9 @@ public class TestJanitor : MonoBehaviour
     }
 
     void FlirtAction()
-    {
-        //Debug.Log("ESPERANDO");
-        
+    {     
         flagForCook = true;
-       // while (Vector3.Distance(transform.position, rcook.transform.position)>1.0f) {}
         StartCoroutine(Flirt());
-        //StopCoroutine(Flirt());
-        //flirt();
-        //flagForCook = false;
     }
 
     void OriginAgainAction()
@@ -239,20 +188,26 @@ public class TestJanitor : MonoBehaviour
         meshAgent.destination = origin;
 
         meshAgent.speed = 3.5f;
-        // meshAgent.destination = posDest;
         arriveToDestination.SetDestination(meshAgent.destination);
         endFlirt = false;
         n = 0;
     }
 
+    bool CookNear()
+    {
+        if (Vector3.Distance(transform.position, cook.transform.position) <= 1f)
+        {
+            return true;
+        }
+        return false;
+    }
+
     IEnumerator Flirt()
     {
-        //Debug.Log("FLIRTEANDO");
         yield return new WaitForSeconds(1.0f);
         yield return new WaitUntil(() => cook.HasReachedDestination());
-        scJan.ShowNewSign(1);
+        scJan.ShowNewSign(1);   
         yield return new WaitForSeconds(4.0f);
-        //Debug.Log("FIN");
         cook.EndJanitor();
         scJan.RemoveSign();
         flagForCook = false;
@@ -266,10 +221,8 @@ public class TestJanitor : MonoBehaviour
         scJan.ShowNewSign(0);
         meshAgent.speed = 0;
         yield return new WaitForSeconds(3.0f);
-        //Debug.Log("FIN");
         scJan.RemoveSign();
         meshAgent.speed = 3.5f;
         fsm.Fire("continue");
-        //endClean = true;
     }
 }
